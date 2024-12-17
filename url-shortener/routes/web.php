208a8781ad953e2017b21ffa1692df4ed1f25188
razamerchantwork <razamerchantwork@gmail.com>
2024-12-17 22:31:58 +0500

    resolve route add in web route file

--- url-shortener/routes/web.php
+++ url-shortener/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlShortenerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{shortId}', [UrlShortenerController::class, 'resolve']);
